81398ea55a4c8866f5b6ee7e26da4452220a106f
Vlad Burma <100062601+axvlad@users.noreply.github.com>
2022-06-20 09:34:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,20 +11,20 @@ namespace GuessNumberGame
             CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
             CancellationToken token = cancelTokenSource.Token;
 
-            //DiligentPlayer Deligent = new DiligentPlayer();
-            //Task t1 = Task.Run(() => Deligent.Roll(cancelTokenSource));
+            DiligentPlayer Deligent = new DiligentPlayer();
+            Task t1 = Task.Run(() => Deligent.Roll(cancelTokenSource));
 
-            //RandomPlayer Random = new RandomPlayer();
-            //Task t2 = Task.Run(() => Random.Roll(cancelTokenSource));
+            RandomPlayer Random = new RandomPlayer();
+            Task t2 = Task.Run(() => Random.Roll(cancelTokenSource));
 
-            //RandomSmartPlayer Random_Smart = new RandomSmartPlayer();
-            //Task t3 = Task.Run(() => Random_Smart.Roll(cancelTokenSource));
+            RandomSmartPlayer Random_Smart = new RandomSmartPlayer();
+            Task t3 = Task.Run(() => Random_Smart.Roll(cancelTokenSource));
 
-            //CheaterPlayer Cheater = new CheaterPlayer();
-            //Task t4 = Task.Run(() => Cheater.Roll(cancelTokenSource));
+            CheaterPlayer Cheater = new CheaterPlayer();
+            Task t4 = Task.Run(() => Cheater.Roll(cancelTokenSource));
 
-            SuperCheaterPlayer Super_Cheater = new SuperCheaterPlayer();
-            Task t5 = Task.Run(() => Super_Cheater.Roll(cancelTokenSource));
+            //SuperCheaterPlayer Super_Cheater = new SuperCheaterPlayer();
+            //Task t5 = Task.Run(() => Super_Cheater.Roll(cancelTokenSource));
 
             Console.ReadLine();
         }
